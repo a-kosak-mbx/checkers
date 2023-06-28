0ee0d7dce824bfe6937498bd0be4195e84ab95f6
Sasha K <a.kosak.mbx@gmail.com>
2023-06-28 21:44:35 +0300

    User info block was update to show dynamic info.

--- players/views.py
+++ players/views.py
@@ -10,12 +10,13 @@ def get_number_of_messages_for_user() -> int:
     return 4
 
 
-def get_short_user_name() -> str:
-    return 'S. Name'
+def get_short_user_name(user) -> str:
+    # print(dir(user))
+    return f"{user.first_name[0] + '. ' if user.first_name else ''}{user.last_name}"
 
 
-def get_long_user_name() -> str:
-    return 'Some Name'
+def get_long_user_name(user) -> str:
+    return f'{user.first_name} {user.last_name}'
 
 
 def get_grade() -> str:
@@ -23,13 +24,16 @@ def get_grade() -> str:
 
 
 def index(request: HttpRequest):
-    context = {
-        'number_of_notifications': get_number_of_notifications_for_user(),
-        'number_of_messages': get_number_of_messages_for_user(),
-        'short_user_name': get_short_user_name(),
-        'long_user_name': get_long_user_name(),
-        'grade': get_grade(),
-    }
+    user = request.user
+    if user.is_authenticated:
+        context = {
+            'number_of_notifications': get_number_of_notifications_for_user(),
+            'number_of_messages': get_number_of_messages_for_user(),
+            'short_user_name': get_short_user_name(user),
+            'long_user_name': get_long_user_name(user),
+            'grade': get_grade(),
+        }
+
     return render(request, 'index.html', context=context)
 
 
